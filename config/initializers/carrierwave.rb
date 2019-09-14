84d160d989bad2bbd22385320c26c154bfaff14f
Emad Masoud <emad3000@gmail.com>
2019-09-14 14:38:11 +0000

    update config file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,19 @@
 # config/initializers/carrierwave.rb
 
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_public = false
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-  }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+if Rails.env.development? || true
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_public = false
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  end
 end
